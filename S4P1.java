8cdae5d868cacd0713c08bd1fa2485131b4ecbe7
baniyanoor-svg <baniyanoor@gmail.com>
2026-02-25 15:52:06 +0530

    Add files via upload

---
+++ S4P1.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+public class S4P1{
+
+public static void main (String[] args){
+
+ArrayList<String> Name = new ArrayList<String>();
+
+
+Name.add("noor");
+Name.add("bhavna");
+Name.add("thv");
+Name.add("akriti");
+Name.add("sug");
+
+//for (String str : Name) {
+  //System.out.println(str); 
+    
+for(int i=0;i<= Name.size(); i++){
+
+
+    System.out.println(Name.get(i));
+}
+
+
+}
+
+}
