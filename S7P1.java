9e1311536b03853544fbabbb0a603ae5c8983f76
baniyanoor-svg <baniyanoor@gmail.com>
2026-03-11 16:20:19 +0530

    Rename Student class to Student1

--- S7P1.java
+++ S7P1.java
@@ -1,8 +1,8 @@
 
-class Student{
+class Student1{
 String name;
 int rollno;
-Student(String n,int r ){
+Student1(String n,int r ){
 name =n;
 rollno=r;
 
@@ -16,7 +16,7 @@ System.out.println("Name:" + name +"Rollno:" + rollno );
 public class S7P1{
 public static void  main(String args[]){
 
-Student st1 =new Student("mohan", 3);
+Student1 st1 =new Student1("mohan", 3);
 st1.display();
 
 }
